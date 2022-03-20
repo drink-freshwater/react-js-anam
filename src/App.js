edbab11c2a1f954853e5af92db739b2e03a0ad74
Chairul Anam <anam.chairul@bintangpelajar.co.id>
2022-03-20 22:26:02 +0700

    komit pertama

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Tujuan Kursus</h2>
+      <ul>
+        <li>Selesaikan Kursus</li>
+        <li>Pelajari Semua Topik Utama Kursus</li>
+        <li>Bantu semua orang di Q&amp;A kursus</li>
+      </ul>
     </div>
   );
 }
