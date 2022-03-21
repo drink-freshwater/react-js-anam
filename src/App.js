34430d888af627349a8aebd97fdf970f4366b8e9
chairulanam612 <chairulanam612@gmail.com>
2022-03-21 11:12:08 +0700

    coba

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <li>Selesaikan Kursus</li>
         <li>Pelajari Semua Topik Utama Kursus</li>
         <li>Bantu semua orang di Q&amp;A kursus</li>
+        <li>Coba komit dan push dari kantor</li>
       </ul>
     </div>
   );
