1909933892d38b3345bd24747f528f87b9e02f78
chairulanam612 <chairulanam612@gmail.com>
2022-03-24 17:30:43 +0700

    handle state form text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import GoalList from "./components/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 function App() {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: "cg1", text: "Selesaikan Kursus" },
     { id: "cg2", text: "Pelajari Semua Topik Utama Kursus" },
     { id: "cg3", text: "Bantu semua orang di Q&A kursus" },
     { id: "cg4", text: "Coba komit dan push dari kantor" },
-  ];
+  ]);
 
   const addNewGoal = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals((prevCourseGoal) => prevCourseGoal.concat(newGoal));
   };
   return (
     <div className="course-goals">
